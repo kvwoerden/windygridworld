646fa368e1505398e0d742ad3ffc175bc9bb2d58
Koen van Woerden <koenvanwoerden@gmail.com>
2019-12-18 14:48:03 +0100

    Implement sarsa agent

--- windy.py
+++ windy.py
@@ -7,6 +7,7 @@ from gym import wrappers, logger
 import gym_windy_gridworlds
 
 import numpy as np
+import random
 
 class RandomAgent(object):
     """The world's simplest agent!"""
@@ -15,6 +16,45 @@ class RandomAgent(object):
 
     def act(self, observation, reward, done):
         return self.action_space.sample()
+
+class KeyboardAgent(object):
+    def __init__(self, action_space):
+        self.action_space = action_space
+        self.actions = { 'w': 'U', 'a': 'L', 's': 'D', 'd': 'R'}
+
+    def act(self):
+        print("Enter one of the keys [a,w,s,d] (lower case) to make a move")
+        key = input()
+        if key in self.actions:
+            return self.actions[key]
+        else:
+            return ''
+
+class SarsaAgent(object):
+    def __init__(self, state_space, action_space):
+        self.state_space = state_space
+        self.action_space = action_space
+        self.actions = { 'w': 'U', 'a': 'L', 's': 'D', 'd': 'R'}
+        self.height = self.state_space[0].n
+        self.width = self.state_space[1].n
+        self.nA = self.action_space.n
+        self.Q = np.zeros((self.height, self.width, self.nA))
+
+    def choose_act(self, state, epsilon):
+        if np.random.random() > epsilon:
+            Qa = self.Q[state[0], state[1]]
+            Qamax = np.max(Qa)
+            action = random.choice(*np.where(Qa == Qamax))
+        else:
+            print("#### Random action")
+            action = np.random.randint(self.nA)
+        return action
+
+    def updateQ(self, state, action, stateprime, actionprime, reward, gamma, alpha):
+        self.Q[state[0], state[1], action] += alpha*(reward + gamma*self.Q[stateprime[0], stateprime[1], actionprime] - self.Q[state[0], state[1], action]) 
+
+        
+
 
 if __name__ == '__main__':
     # parser = argparse.ArgumentParser(description=None)
@@ -36,24 +76,45 @@ if __name__ == '__main__':
     # outdir = './video/random-agent-results'
     # env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
-    agent = RandomAgent(env.action_space)
+    # agent = RandomAgent(env.action_space)
+    agent = SarsaAgent(env.observation_space, env.action_space)
 
-    episode_count = 2
+    episode_count = 10000
     reward = 0
     done = False
+    EPSILON = 0.1
+    GAMMA = 1
+    ALPHA = 0.5
+    steps = np.zeros((episode_count, 2))
+    steps[:, 0] = np.arange(episode_count)
+    INTERVAL = 500
 
     for i in range(episode_count):
-        ob = env.reset()
+        state = env.reset()
+        action = agent.choose_act(state, EPSILON)
+
         while True:
+            if i % INTERVAL == 0:
+                env.render()
+            # print("Currently at position: ", env.observation)
+            stateprime, reward, done, _ = env.step(action)
+            steps[i, 1] += 1
             # env.render()
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
-            # print("###")
-            # print("ob: ", ob)
-            # print("reward:", reward)
-            # print(":done: ", done)
-            env.render()
+            # action = agent.act(ob, reward, done)
+            actionprime = agent.choose_act(stateprime, EPSILON)
+            agent.updateQ(state, action, stateprime, actionprime, reward, GAMMA, ALPHA)
+            state = stateprime
+            action = actionprime
             if done:
+                if i % INTERVAL == 0:
+                    env.render()
+                    print("Done with episode " + str(i))
+                    print("This took " + str(steps[i, 1]) + " steps")
+                    print("Pick new epsilon")
+                    epsilon_string = input()
+                    if epsilon_string != '':
+                        EPSILON = float(epsilon_string)
+                print("Done with episode " + str(i))
                 break
             # Note there's no env.render() here. But the environment still can open window and
             # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
@@ -62,3 +123,4 @@ if __name__ == '__main__':
     # Close the env and write monitor result info to disk
            
     env.close()
+    print(steps[-20:,:])
